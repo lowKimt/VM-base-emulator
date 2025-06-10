28c3e3a063ca1804026ce5b2261b6b726fc7e251
lowKimt <lowkimt5@gmail.com>
2025-06-10 20:29:02 +0800

    Do optimizations, using command numbers as keys directly

--- src/vm.lua
+++ src/vm.lua
@@ -2,6 +2,10 @@ local VM = {}
 
 VM.COMMANDS = {0x01, 0x02, 0x03}
 
+-- 0x01 PUSH
+-- 0x02 POP
+-- 0x03 PRINT/LOG
+
 function VM:new()
     local vm = setmetatable({}, {__index = self})
     vm:reset()
@@ -9,18 +13,19 @@ function VM:new()
 end
 
 function VM:reset()
-    self.Stack = {}
+    -- self.Stack = {} -- Unused for now; Do not Delete!
     self.Memory = {}
 end
 
 VM.Handlers = {
-    [VM.COMMANDS[1]] = function(self, arg)
+    -- self.Memory[0] is a placeholder, I will change this to be more flexible.
+    [0x01] = function(self, arg)
         self.Memory[0] = arg
     end,
-    [VM.COMMANDS[2]] = function(self)
+    [0x02] = function(self)
         self.Memory[0] = 0
     end,
-    [VM.COMMANDS[3]] = function(self)
+    [0x03] = function(self)
         print(tostring(self.Memory[0]))
     end
 }
@@ -29,6 +34,7 @@ function VM:exec(command, arg)
     local handler = self.Handlers[command]
     if not handler then
         print("Command does not exist: " .. tostring(command))
+        return -- add return
     end
     handler(self, arg)
 end
