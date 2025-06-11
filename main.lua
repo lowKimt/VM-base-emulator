e219ffa36943c0c19d59fc6280241dbbee7f00ac
lowKimt <lowkimt5@gmail.com>
2025-06-11 09:29:19 +0800

    Update to use more clear commands by direct VM.COMMANDS

--- main.lua
+++ main.lua
@@ -1,15 +1,15 @@
-local VM = require("src/vm")
+local VM = require("src.vm")
 
 local vm = VM:new()
 
 -- Memory Index 0 = 10
-vm:exec(0x01, 0, 10)
+vm:exec(VM.COMMANDS.OPCODE_PUSH, 0, 10)
 
 -- Print Index 0 (10)
-vm:exec(0x03, 0)
+vm:exec(VM.COMMANDS.OPCODE_LOG, 0)
 
 -- POP Index 0 (to 0)
-vm:exec(0x02, 0)
+vm:exec(VM.COMMANDS.OPCODE_POP, 0)
 
 -- Print Index 0 (0)
-vm:exec(0x03, 0)
+vm:exec(VM.COMMANDS.OPCODE_LOG, 0)
