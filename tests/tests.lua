52a8d68b77c539358aaef8992ea4c357ea594f32
lowKimt <lowkimt5@gmail.com>
2025-06-12 18:07:02 +0800

    Edit tests to be latest

--- tests/tests.lua
+++ tests/tests.lua
@@ -3,9 +3,15 @@ local VM = require(".src.vm")
 
 local vm = VM:new()
 
--- Push 5, Push 10, Add, Pop to Register[1], Log Register[1]
+-- spush 5, spush 10, add, spush 200, spush 5, div
 vm:exec(vm.COMMANDS.OPCODE_STACK_PUSH, nil, 5)
 vm:exec(vm.COMMANDS.OPCODE_STACK_PUSH, nil, 10)
 vm:exec(vm.COMMANDS.OPCODE_ADD)
+vm:exec(vm.COMMANDS.OPCODE_STACK_PUSH, nil, 200)
+vm:exec(vm.COMMANDS.OPCODE_STACK_PUSH, nil, 2)
+vm:exec(vm.COMMANDS.OPCODE_DIV)
+-- spop [1], spop [2], log [1], log [2]
 vm:exec(vm.COMMANDS.OPCODE_STACK_POP, 1)
-vm:exec(vm.COMMANDS.OPCODE_LOG, 1)  -- Should print: 15
+vm:exec(vm.COMMANDS.OPCODE_STACK_POP, 2)
+vm:exec(vm.COMMANDS.OPCODE_LOG, 1)
+vm:exec(vm.COMMANDS.OPCODE_LOG, 2)
